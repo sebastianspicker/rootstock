8bf71b5a0c61cb805156a3a96702d1efa021336d
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-08-03 23:15:00 +0200

    feat(api): expose graph queries over a FastAPI surface

---
+++ graph/api.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def build_api_summary() -> dict[str, str]:
+    return {"scope": "api", "status": "ready"}
+
+# current lane: api
+def api_task() -> dict[str, str]:
+    return {"scope": "api", "status": "ready"}
